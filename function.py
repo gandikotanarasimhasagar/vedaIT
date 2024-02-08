3c1a9ee447e148adf8147e2bceab634b2e128ac3
sagar <narasimhasagar770@gmail.com>
2024-02-08 11:23:13 +0530

    added a new file

---
+++ function.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  8 11:19:50 2024
+
+@author: naras
+"""
+
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
